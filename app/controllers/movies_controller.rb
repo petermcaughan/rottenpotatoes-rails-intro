4ec8a6dfe463c19516ee0a0d6740251ba87fbc36
petermcaughan <peter.mcaughan@gmail.com>
2018-02-16 13:15:44 -0600

    Part 2 Deployment Test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-     #Check to see if clicked params were title or release date, change css and ordering respectively
+    #Initialize this so index.html.haml can create button for each rating
+    @all_ratings = ['G','PG','PG-13','R']
+
+     #Check to see if clicked params were title or release date, change css and ordering of Movie respectively
      if params[:key] == 'title'
        @title_css = 'hilite'
        @movies = Movie.order(title: :asc)
      elsif params[:key] == 'release_date'
        @release_css = 'hilite'		
        @movies = Movie.order(release_date: :asc)
-     #If neither title or release date were clicked, we don't care.
      else
-       @movies = Movie.all	
+       #If we got here the user may or may not have clicked refresh with some boxes checked. First, check to see if they wanted to filter based on rating
+       if (params[:ratings].nil?)
+         @movies = Movie.all
+       else
+       #Grab the keys [NOT VALUES] from params[:ratings] to find desired ratings, and display movies
+       #where the rating is found in the keys
+       @rating_filter = params[:ratings].keys
+       @movies = Movie.where(rating: @rating_filter)
+       end
      end
   end
 
